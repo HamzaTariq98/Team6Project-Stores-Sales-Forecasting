64d48722c00269427e9fcf0d872b42e89786ec0b
HamzaTariq98 <hamzatarq98@gmail.com>
2023-09-14 13:51:39 +0300

    update

--- web_server.py
+++ web_server.py
@@ -8,13 +8,12 @@ app = Flask(__name__)
 
 
 def model_function(x1):
-    # x1 = pd.DataFrame(x1, columns=['Store','Dept','Year','Month','IsHoliday','Type','Size','Temperature','Fuel_Price','MarkDown1','MarkDown2','MarkDown3','MarkDown4','MarkDown5','CPI','Unemployment'])
+    x1 = pd.DataFrame(x1, columns=['Store','Dept','Year','Month','IsHoliday','Type','Size','Temperature','Fuel_Price','MarkDown1','MarkDown2','MarkDown3','MarkDown4','MarkDown5','CPI','Unemployment'])
        
-    # loaded_model = joblib.load('.//Models//decision_tree_model.pkl')
-    # loaded_model_incoder = joblib.load('.//Models//incoder_model.pkl')
-    # x1 = loaded_model_incoder.transform(x1)
-    # result = loaded_model.predict(x1)[0]
-    result = ':S'
+    loaded_model = joblib.load('.//Models//decision_tree_model.pkl')
+    loaded_model_incoder = joblib.load('.//Models//incoder_model.pkl')
+    x1 = loaded_model_incoder.transform(x1)
+    result = loaded_model.predict(x1)[0]
     return result
 
 
@@ -25,113 +24,41 @@ def main_page():
 @app.route("/model_run", methods=['POST'])
 def model_run():
 
-    # Get user inputs from the HTML form
-
-    # Store
     Store = request.form.get('Store')
-    if Store == None or Store == '':
-        Store = 10
-    else:
-        Store = float(Store)
-        df = pd.read_csv('.\static\stores.csv')
-        Type = df[df['Store']==Store]['Type'].values[0]
-        Size = df[df['Store']==Store]['Size'].values[0]
+    Store = float(Store)
+    df = pd.read_csv('.\static\stores.csv')
+    Type = df[df['Store']==Store]['Type'].values[0]
+    Size = df[df['Store']==Store]['Size'].values[0]
 
-    # Dept
-    Dept = request.form.get('Dept')
-    if Dept == None or Dept == '':
-        Dept = 10
-    else:
-        Dept = float(Dept)
 
-    # Year
+    Dept = float(request.form.get('Dept'))
     Year = float(request.form.get('Year'))
-
-    # Month
     Month = float(request.form.get('Month'))
+    Temperature = float(request.form.get('Temperature'))
+    Fuel_Price = float(request.form.get('Fuel_Price'))
+    MarkDown1 = float(request.form.get('MarkDown1'))
+    MarkDown2 = float(request.form.get('MarkDown2'))
+    MarkDown3 = float(request.form.get('MarkDown3'))
+    MarkDown4 = float(request.form.get('MarkDown4'))
+    MarkDown5 = float(request.form.get('MarkDown5'))
+    CPI = float(request.form.get('CPI'))
+    Unemployment = float(request.form.get('Unemployment'))
 
 
-    # IsHoliday
     IsHoliday = request.form.get('IsHoliday')
     if IsHoliday == 'Yes':
         IsHoliday = float(1)
     else:
          IsHoliday = float(0)
 
-
-
-    # Temperature
-    Temperature = request.form.get('Temperature')
-    if Temperature == None or Temperature == '':
-        Temperature = 10
-    else:
-        Temperature = float(Temperature)
-
-    # Fuel_Price
-    Fuel_Price = request.form.get('Fuel_Price')
-    if Fuel_Price == None or Fuel_Price == '':
-        Fuel_Price = 10
-    else:
-        Fuel_Price = float(Fuel_Price)
-
-    # MarkDown1
-    MarkDown1 = request.form.get('MarkDown1')
-    if MarkDown1 == None or MarkDown1 == '':
-        MarkDown1 = 10
-    else:
-        MarkDown1 = float(MarkDown1)
-
-    # MarkDown2
-    MarkDown2 = request.form.get('MarkDown2')
-    if MarkDown2 == None or MarkDown2 == '':
-        MarkDown2 = 10
-    else:
-        MarkDown2 = float(MarkDown2)
-
-    # MarkDown3
-    MarkDown3 = request.form.get('MarkDown3')
-    if MarkDown3 == None or MarkDown3 == '':
-        MarkDown3 = 10
-    else:
-        MarkDown3 = float(MarkDown3)
-
-    # MarkDown4
-    MarkDown4 = request.form.get('MarkDown4')
-    if MarkDown4 == None or MarkDown4 == '':
-        MarkDown4 = 10
-    else:
-        MarkDown4 = float(MarkDown4)
-
-    # MarkDown5
-    MarkDown5 = request.form.get('MarkDown5')
-    if MarkDown5 == None or MarkDown5 == '':
-        MarkDown5 = 10
-    else:
-        MarkDown5 = float(MarkDown5)
-
-    # CPI
-    CPI = request.form.get('CPI')
-    if CPI == None or CPI == '':
-        CPI = 10
-    else:
-        CPI = float(CPI)
-
-    # Unemployment
-    Unemployment = request.form.get('Unemployment')
-    if Unemployment == None or Unemployment == '':
-        Unemployment = 10
-    else:
-        Unemployment = float(Unemployment)
-
-
     x1 = [[Store,Dept,Year,Month,IsHoliday,Type,Size,Temperature,Fuel_Price,MarkDown1,MarkDown2,MarkDown3,MarkDown4,MarkDown5,CPI,Unemployment]]
 
 
     result = model_function(x1)  
-    # result = f"${result:,.0f}"
+    result = f"${result:,.0f}"
 
-    return render_template('index.html', result=result)
-# , Store=Store,Dept=Dept,Year=Year,Month=Month,IsHoliday=IsHoliday,Temperature=Temperature,Fuel_Price=Fuel_Price,MarkDown1=MarkDown1,MarkDown2=MarkDown2,MarkDown3=MarkDown3,MarkDown4=MarkDown4,MarkDown5=MarkDown5,CPI=CPI,Unemployment=Unemployment
+    return render_template('index.html', result=result, Store=Store,Dept=Dept,Year=Year,Month=Month,IsHoliday=IsHoliday,Temperature=Temperature,Fuel_Price=Fuel_Price,MarkDown1=MarkDown1,MarkDown2=MarkDown2,MarkDown3=MarkDown3,MarkDown4=MarkDown4,MarkDown5=MarkDown5,CPI=CPI,Unemployment=Unemployment)
+
 if __name__ == "__main__":
     app.run(debug=True)
 
