47e71402a81c1162b5de97f5e04afaf2187172d4
HamzaTariq98 <hamzatarq98@gmail.com>
2023-09-17 12:45:27 +0300

    update

--- web_server.py
+++ web_server.py
@@ -7,11 +7,32 @@ app = Flask(__name__)
 
 
 
-def model_function(x1):
+def find_actual(Store,Dept,Year,Month):
+    df = pd.read_csv('./static/stores_avg_actual.csv')
+    
+    filtered_df = df[(df['Store'] == Store) & (df['Dept'] == Dept) & (df['Year'] == Year) & (df['Month'] == Month)]
+    
+    if filtered_df.empty:
+        return False
+    
+    return filtered_df['Weekly_Sales'].values[0]
+
+
+def dicision_tree_model_run(x1):
     x1 = pd.DataFrame(x1, columns=['Store','Dept','Year','Month','IsHoliday','Type','Size','Temperature','Fuel_Price','MarkDown1','MarkDown2','MarkDown3','MarkDown4','MarkDown5','CPI','Unemployment'])
        
     loaded_model = joblib.load('.//Models//decision_tree_model.pkl')
     loaded_model_incoder = joblib.load('.//Models//incoder_model.pkl')
+    x1 = loaded_model_incoder.transform(x1)
+    result = loaded_model.predict(x1)[0]
+    return result
+
+
+def linear_tree_model_run(x1):
+    x1 = pd.DataFrame(x1, columns=['Store','Dept','Year','Month','IsHoliday','Type','Size','Temperature','Fuel_Price','MarkDown1','MarkDown2','MarkDown3','MarkDown4','MarkDown5','CPI','Unemployment'])
+       
+    loaded_model = joblib.load('.//Models//linear_reg_model.pkl')
+    loaded_model_incoder = joblib.load('.//Models//incoder_model_onehot.pkl')
     x1 = loaded_model_incoder.transform(x1)
     result = loaded_model.predict(x1)[0]
     return result
@@ -56,11 +77,19 @@ def model_run():
 
     x1 = [[Store,Dept,Year,Month,IsHoliday,Type,Size,Temperature,Fuel_Price,MarkDown1,MarkDown2,MarkDown3,MarkDown4,MarkDown5,CPI,Unemployment]]
 
+    result_actual = find_actual(Store,Dept,Year,Month)
+    if result_actual is False:
+        result_actual = 'NA'
+    else:
+        result_actual = f"${result_actual:,.0f}"
 
-    result = model_function(x1)  
-    result = f"${result:,.0f}"
+    result_tree = dicision_tree_model_run(x1)  
+    result_tree = f"${result_tree:,.0f}"
 
-    return render_template('index.html', result=result, Store=Store,Dept=Dept,Year=Year,Month=Month,IsHoliday=IsHoliday,Temperature=Temperature,Fuel_Price=Fuel_Price,MarkDown1=MarkDown1,MarkDown2=MarkDown2,MarkDown3=MarkDown3,MarkDown4=MarkDown4,MarkDown5=MarkDown5,CPI=CPI,Unemployment=Unemployment, flag = 1)
+    result_linear = linear_tree_model_run(x1)  
+    result_linear = f"${result_linear:,.0f}"
+
+    return render_template('index.html',result_actual=result_actual, result_linear=result_linear,result_tree=result_tree, Store=Store,Dept=Dept,Year=Year,Month=Month,IsHoliday=IsHoliday,Temperature=Temperature,Fuel_Price=Fuel_Price,MarkDown1=MarkDown1,MarkDown2=MarkDown2,MarkDown3=MarkDown3,MarkDown4=MarkDown4,MarkDown5=MarkDown5,CPI=CPI,Unemployment=Unemployment)
 
 if __name__ == "__main__":
     app.run(debug=True)
