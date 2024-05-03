c0b29dbaab1c34ad10eabeaf8f94b154d6662160
HamzaTariq98 <hamzatarq98@gmail.com>
2024-05-03 19:31:54 +0300

    switch for linear and free plan

--- web_server.py
+++ web_server.py
@@ -1,9 +1,9 @@
 import joblib
 import time
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from flask import Flask, render_template, request
-from statsmodels.tsa.statespace.sarimax import SARIMAX
+# from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 app = Flask(__name__)
 
@@ -24,58 +24,60 @@ def dicision_tree_model_run(x1):
     x1 = pd.DataFrame(x1, columns=['Store', 'Dept', 'Year', 'Month', 'IsHoliday', 'Type', 'Size', 'Temperature',
                       'Fuel_Price', 'MarkDown1', 'MarkDown2', 'MarkDown3', 'MarkDown4', 'MarkDown5', 'CPI', 'Unemployment'])
 
-    loaded_model = joblib.load('.//Models//decision_tree_model.pkl')
+    # loaded_model = joblib.load('.//Models//decision_tree_model.pkl')
+
+    loaded_model = joblib.load('.//Models//linear_reg_model.pkl')
     loaded_model_incoder = joblib.load('.//Models//incoder_model.pkl')
     x1 = loaded_model_incoder.transform(x1)
     result = loaded_model.predict(x1)[0]
     return result
 
 
-def SARIMA_model_run(*, DataFrame, Store, Month, Year):
-    DataFrame.Date = pd.to_datetime(DataFrame.Date)
-    df_forecast = DataFrame[(DataFrame.Store == Store)]
-    ts = df_forecast.groupby('Date')['Weekly_Sales'].mean()
-    ts_train = ts.head(120)
-    ts_test = ts.tail(23)
-    order = (1, 1, 1)
-    seasonal_order = (1, 1, 1, 52)
-    # print (f'ts_train shape: {ts_train.shape}, ts_test shape: {ts_test.shape}')
+# def SARIMA_model_run(*, DataFrame, Store, Month, Year):
+#     DataFrame.Date = pd.to_datetime(DataFrame.Date)
+#     df_forecast = DataFrame[(DataFrame.Store == Store)]
+#     ts = df_forecast.groupby('Date')['Weekly_Sales'].mean()
+#     ts_train = ts.head(120)
+#     ts_test = ts.tail(23)
+#     order = (1, 1, 1)
+#     seasonal_order = (1, 1, 1, 52)
+#     # print (f'ts_train shape: {ts_train.shape}, ts_test shape: {ts_test.shape}')
 
-    sarima_model = SARIMAX(ts_train, order=order,
-                           seasonal_order=seasonal_order)
-    sarima_results = sarima_model.fit()
+#     sarima_model = SARIMAX(ts_train, order=order,
+#                            seasonal_order=seasonal_order)
+#     sarima_results = sarima_model.fit()
 
-    # print(sarima_results.summary())
-    forecast_period = 85
-    forecast = sarima_results.get_forecast(steps=forecast_period)
-    forecasted_values = forecast.predicted_mean
-    filtered = forecasted_values[(forecasted_values.index.year == Year) & (
-        forecasted_values.index.month == Month)]
-    flag = 0
-    if filtered.empty:
-        filtered = ts_train[(ts_train.index.year == Year)
-                            & (ts_train.index.month == Month)]
-        flag = 1
-    # filtered= forecasted_values.between(f'{Year}-{Month}-01',f'{Year}-{Month}-31')
+#     # print(sarima_results.summary())
+#     forecast_period = 85
+#     forecast = sarima_results.get_forecast(steps=forecast_period)
+#     forecasted_values = forecast.predicted_mean
+#     filtered = forecasted_values[(forecasted_values.index.year == Year) & (
+#         forecasted_values.index.month == Month)]
+#     flag = 0
+#     if filtered.empty:
+#         filtered = ts_train[(ts_train.index.year == Year)
+#                             & (ts_train.index.month == Month)]
+#         flag = 1
+#     # filtered= forecasted_values.between(f'{Year}-{Month}-01',f'{Year}-{Month}-31')
 
-    forecast_conf_int = forecast.conf_int()
-    ts_pred = []
-    for i in ts_test.index:
-        ts_pred.append(sarima_results.predict(pd.Timestamp(i)))
+#     forecast_conf_int = forecast.conf_int()
+#     ts_pred = []
+#     for i in ts_test.index:
+#         ts_pred.append(sarima_results.predict(pd.Timestamp(i)))
 
-    plt.figure(figsize=(12, 6))
-    plt.plot(ts, label='Observed')
-    plt.plot(forecast.predicted_mean, label='Forecast', color='red')
-    plt.fill_between(forecast_conf_int.index,
-                     forecast_conf_int.iloc[:, 0], forecast_conf_int.iloc[:, 1], color='pink')
-    plt.axvline(ts_train.tail(1).index, color='orange')
-    plt.title('SARIMA Forecast for Store {}'.format(Store))
-    plt.legend()
-    plt.savefig('.//static//figure1.jpg')
-    if flag:
-        return f'${filtered.mean():,.0f} (Actual)'
-    else:
-        return f'${filtered.mean():,.0f} (Forecasted)'
+#     plt.figure(figsize=(12, 6))
+#     plt.plot(ts, label='Observed')
+#     plt.plot(forecast.predicted_mean, label='Forecast', color='red')
+#     plt.fill_between(forecast_conf_int.index,
+#                      forecast_conf_int.iloc[:, 0], forecast_conf_int.iloc[:, 1], color='pink')
+#     plt.axvline(ts_train.tail(1).index, color='orange')
+#     plt.title('SARIMA Forecast for Store {}'.format(Store))
+#     plt.legend()
+#     plt.savefig('.//static//figure1.jpg')
+#     if flag:
+#         return f'${filtered.mean():,.0f} (Actual)'
+#     else:
+#         return f'${filtered.mean():,.0f} (Forecasted)'
 
 
 @app.route("/")
@@ -126,16 +128,16 @@ def model_run():
     SARIMA_result = 0
     result_tree = 0
 
-    if request.form.get('modelSelect') == 'SARIMA':
-        flag = 0
-        df = pd.read_csv('./static/time_series.csv')
-        SARIMA_result = SARIMA_model_run(
-            DataFrame=df, Store=Store, Month=Month, Year=Year)
+    # if request.form.get('modelSelect') == 'SARIMA':
+    #     flag = 0
+    #     df = pd.read_csv('./static/time_series.csv')
+    #     SARIMA_result = SARIMA_model_run(
+    #         DataFrame=df, Store=Store, Month=Month, Year=Year)
 
-    else:
-        flag = 1
-        result_tree = dicision_tree_model_run(x1)
-        result_tree = f"${result_tree:,.0f}"
+    # else:
+    flag = 1
+    result_tree = dicision_tree_model_run(x1)
+    result_tree = f"${result_tree:,.0f}"
 
     return render_template('index.html', flag=flag, result_actual=result_actual, SARIMA_result=SARIMA_result, result_tree=result_tree, Store=Store, Dept=Dept, Year=Year, Month=Month, IsHoliday=IsHoliday, Temperature=Temperature, Fuel_Price=Fuel_Price, MarkDown1=MarkDown1, MarkDown2=MarkDown2, MarkDown3=MarkDown3, MarkDown4=MarkDown4, MarkDown5=MarkDown5, CPI=CPI, Unemployment=Unemployment)
 
